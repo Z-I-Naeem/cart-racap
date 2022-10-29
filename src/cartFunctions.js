6ad70e6b99e6b63919983e9c842f5af76445081a
Z-I-Naeem <md.zahidulislamnaeem81@gmail.com>
2022-10-29 18:19:22 +0600

    code shorted

--- src/cartFunctions.js
+++ src/cartFunctions.js
@@ -4,13 +4,14 @@ let count = null
 let addToCart = id => {
     // console.log('item added' , id);
     const item = localStorage.getItem(id)
-    if(item){
-        console.log(true);
-        console.log(count++);
-    }else{
-        count = 1
-    }
-    console.log(count);
+    // if(item){
+    //     console.log(true);
+    //     console.log(count++);
+    // }else{
+    //     count = 1
+    // }
+    item?count++:count = 1
+    // console.log(count);
     localStorage.setItem(id,JSON.stringify(count))
 }
 export default addToCart
